b492a38fe5124b5fbb3df8c12cbe68583a1dfefc
Barry Keith Salmon <salmon.barry1@gmail.com>
2024-07-26 17:58:03 +0700

    Continue wit Day 1 of Python Training

--- Section01/Day01/Lesson01.py
+++ Section01/Day01/Lesson01.py
@@ -1,1 +1,9 @@
-print("Hello World")
+# This is a comment
+print("Hello World\nHello World! "+ input("What is your name?"))
+
+num1 = int(input("First Number: "))
+num2 = int(input("Second Number: "))
+
+print(num1 + num2)
+
+print(len(input()))
