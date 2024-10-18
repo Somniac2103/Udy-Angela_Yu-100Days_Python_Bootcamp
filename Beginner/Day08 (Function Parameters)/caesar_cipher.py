c13c737e906c5b7797a813e4b0daf2a45b304653
Barry Keith Salmon <salmon.barry1@gmail.com>
2024-10-18 18:57:58 +0800

    continue with caesar_cipher code

--- Beginner/Day08 (Function Parameters)/caesar_cipher.py
+++ Beginner/Day08 (Function Parameters)/caesar_cipher.py
@@ -1,3 +1,17 @@
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u', 'v','w','x','y','z']
 
-direction
+direction = input("Type encode or decode:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def coder(direction,text, shift):
+  codeword = ""
+  for letter in text:
+    if (direction == 'encode'):
+      codeword = codeword + alphabet[(alphabet.index(letter)+shift) % len(alphabet)]
+    elif(direction == 'decode'):
+      codeword = codeword + alphabet[(alphabet.index(letter)-shift) % len(alphabet)]
+  return print(f'here is your results: {codeword}')
+
+coder(direction=direction,text=text, shift=shift)
