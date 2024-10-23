318be7ea563a09b13979393ee95031b6ca52391b
Barry Keith Salmon <salmon.barry1@gmail.com>
2024-10-23 14:59:45 +0800

    Completed day 08

--- Beginner/Day08 (Function Parameters)/caesar_cipher.py
+++ Beginner/Day08 (Function Parameters)/caesar_cipher.py
@@ -1,17 +1,24 @@
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u', 'v','w','x','y','z']
 
-direction = input("Type encode or decode:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
+repeat = 'yes'
 
 def coder(direction,text, shift):
   codeword = ""
-  for letter in text:
-    if (direction == 'encode'):
-      codeword = codeword + alphabet[(alphabet.index(letter)+shift) % len(alphabet)]
-    elif(direction == 'decode'):
-      codeword = codeword + alphabet[(alphabet.index(letter)-shift) % len(alphabet)]
-  return print(f'here is your results: {codeword}')
+  if (direction == 'decode'):
+          shift = -1 * shift
+  for letter in text:   
+      if letter in alphabet:
+        codeword = codeword + alphabet[(alphabet.index(letter)+shift) % len(alphabet)]
+      else: 
+        codeword = codeword + letter           
+  return print(f'Here is your {direction}d results: {codeword}')
 
-coder(direction=direction,text=text, shift=shift)
+while repeat == 'yes':
+  direction = input("Type encode or decode:\n").lower()
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  coder(direction=direction,text=text, shift=shift)
+  repeat = input("Would you like to use again: yes or no?\n").lower()
+
+print("Thank you for using my cyther code, goodbye")
+  
