97dc15546cd372b30368ff7c8782a1969e899dd0
Barry Keith Salmon <salmon.barry1@gmail.com>
2024-12-16 22:18:02 +0000

    Continue with python course

---
+++ Intermediate/Day16 (OOP)/turtle_Exercise.py
@@ -0,0 +1,20 @@
+# import turtle
+# timmy = turtle.Turtle()
+
+# from turtle import Turtle, Screen
+
+
+# from turtle import Turtle
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
