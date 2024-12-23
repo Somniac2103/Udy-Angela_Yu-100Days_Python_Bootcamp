8de759df3280d4d7c120591241df70e2ccbc430b
Barry Keith Salmon <salmon.barry1@gmail.com>
2024-12-23 18:03:32 +0000

    Commence with OOP Coffee Machine

--- Intermediate/Day16 (OOP)/oop-coffee-machine-start/main.py
+++ Intermediate/Day16 (OOP)/oop-coffee-machine-start/main.py
@@ -1,3 +1,21 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+# User input
+available_Coffee = Menu()
+
+selected_coffee = input("Select desired coffee? " + available_Coffee.get_items()+"\n")
+chosen_coffee = available_Coffee.find_drink(selected_coffee)
+print(chosen_coffee.name)
+# Turn off function
+if
+# Report Print Function
+
+# Resource Checker Function
+
+# Coin Processor Function
+
+# Transaction checker Function
+
+# Make coffee
