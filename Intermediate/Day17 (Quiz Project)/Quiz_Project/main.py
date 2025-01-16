12934486389ddd7e8d5ad441f9bdf79f766833cd
Barry Keith Salmon <salmon.barry1@gmail.com>
2025-01-16 18:15:30 +0000

    Completed day 17

--- Intermediate/Day17 (Quiz Project)/Quiz_Project/main.py
+++ Intermediate/Day17 (Quiz Project)/Quiz_Project/main.py
@@ -12,4 +12,8 @@ quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
     quiz.next_question()
+
+print("You'v completed the quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+
         
