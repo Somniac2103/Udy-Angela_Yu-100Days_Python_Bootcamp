ec41beaedd00ee51f6ec09ec26a7a3adc38fa6b8
Barry Keith Salmon <salmon.barry1@gmail.com>
2025-01-17 14:23:58 +0000

    Completed Lesson 132

--- Intermediate/Day18(GUI)/main.py
+++ Intermediate/Day18(GUI)/main.py
@@ -4,6 +4,23 @@ timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("green")
 
+# for _  in range(4):
+#     timmy_the_turtle.forward(300)
+#     timmy_the_turtle.left(90)
+
+for steps in range(50):
+    for _ in range(5):
+        timmy_the_turtle.penup()
+        timmy_the_turtle.forward(2)
+        timmy_the_turtle.pendown()
+        timmy_the_turtle.forward(2)
+    timmy_the_turtle.penup()
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.pendown()
+    timmy_the_turtle.forward(10)
+
+
+        
 
 screen = Screen()
 screen.exitonclick()
