d0365f343695b291377befe2acd327ddd151c0ef
Barry Keith Salmon <salmon.barry1@gmail.com>
2025-01-23 15:58:12 +0000

    Almost complete with hirch paints

--- Intermediate/Day18(GUI)/Hirst Project/main.py
+++ Intermediate/Day18(GUI)/Hirst Project/main.py
@@ -1,4 +1,6 @@
 import colorgram
+from turtle import Turtle, Screen
+from random import randint
 
 rgb_colors = []
 
@@ -13,3 +15,42 @@ for color in colors:
  
 
 print(rgb_colors)
+
+timmy = Turtle()
+screen = Screen()
+screen.colormode(255)
+
+row = 5
+column = 5
+
+circle_range = 5
+spacer = 50
+
+for i in range(column):
+    if i%2:
+        timmy.penup()
+        timmy.right(90)
+        timmy.forward(spacer)
+        timmy.right(90)
+        timmy.forward(spacer)
+    else:
+        timmy.penup()
+        timmy.left(90)
+        timmy.forward(spacer + 2*circle_range)
+        timmy.left(90)
+        timmy.forward(spacer)
+
+    for j in range(int(row)):        
+        number = randint(1, 29)
+        selectedColor = (rgb_colors[number][0],rgb_colors[number][1],rgb_colors[number][2])
+        timmy.color(selectedColor)
+        timmy.pendown()
+        timmy.circle(circle_range)
+        timmy.penup()
+        timmy.forward(spacer)
+        
+
+screen.exitonclick()
+
+
+
