6e55941da34d69ae4511bf49fc02f35a7af66aa9
Barry Keith Salmon <salmon.barry1@gmail.com>
2025-01-28 11:58:18 +0000

    Busy with lesson 147

---
+++ Intermediate/Day20 (Snake Game)/main.py
@@ -0,0 +1,30 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+snake_body = []
+
+
+
+# for snake_part in range(0, 3):
+#     new_snakebody = Turtle(shape= "square")
+#     new_snakebody.color = "white"
+#     new_snakebody.goto(x=(-20*snake_part), y=0)
+#     snake_body.append(new_snakebody)
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
