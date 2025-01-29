1ff349a893fa51830e13c37408a2c8cf0bb6fe9c
Barry Keith Salmon <salmon.barry1@gmail.com>
2025-01-29 12:48:32 +0000

    Complete Lesson 148 and 149

--- Intermediate/Day20 (Snake Game)/main.py
+++ Intermediate/Day20 (Snake Game)/main.py
@@ -1,28 +1,23 @@
 from turtle import Screen, Turtle
+import time
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
-
-snake_body = []
-
+screen.tracer(0)
 
 
-# for snake_part in range(0, 3):
-#     new_snakebody = Turtle(shape= "square")
-#     new_snakebody.color = "white"
-#     new_snakebody.goto(x=(-20*snake_part), y=0)
-#     snake_body.append(new_snakebody)
+snake = Snake()
 
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
 
-
-
-
-
-
-
-
+    snake.move() 
+    
 
 
 
