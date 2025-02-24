1a0e708e3837ba85c846c8998d157d7e09e5edf7
Barry Keith Salmon <salmon.barry1@gmail.com>
2025-02-24 11:14:00 +0000

    State comparator complete

--- Intermediate/Day25 (Panda Lib)/US States Quiz/main.py
+++ Intermediate/Day25 (Panda Lib)/US States Quiz/main.py
@@ -1,4 +1,5 @@
-import turtle
+import turtle 
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -15,10 +16,39 @@ turtle.shape(image)
 
 # turtle.mainloop()
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+FONT = ("Courier", 24, "normal")
 
-# TODO Convert to Title case
-# TODO Check guess with 50 states
+class Statename(turtle.Turtle):
+     def __init__(self):
+        super().__init__()
+        self.name = index.state
+        self.x = index.x
+        self.y = index.y
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.goto(self.x, self.y)
+        self.write(self.name, align="center", font=FONT)   
+   
+
+answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+answer_state = answer_state.capitalize()
+
+data = pandas.read_csv("50_states.csv")
+# print(data)
+
+index = (data[data.state == answer_state])
+# print(index)
+
+if index.empty:
+    print("No state Found")    
+else:
+    print("State Found")
+    Statename(index)
+    
+
+
+
 # TODO Write correct quesses on map
 # TODO Use loop to allow player to keep quessing
 # TODO Record the correct quesses in list
