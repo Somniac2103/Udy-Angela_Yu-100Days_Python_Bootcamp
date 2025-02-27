a241cab4cce286816556f9ef8546f19e18f5ad70
Barry Keith Salmon <salmon.barry1@gmail.com>
2025-02-27 15:41:59 +0000

    Completed list, dict and dataframe comprehension

---
+++ Intermediate/Day26 (List and Dictionary)/dict/main.py
@@ -0,0 +1,21 @@
+import random
+
+# names = [ "Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+
+# students_scores = {student:random.randint(1, 100) for student in names }
+# print(students_scores)
+
+# passed_students = {student:value for (student,value) in students_scores.items() if value >= 60}
+# print(passed_students)
+
+# sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# words = sentence.split()
+# print(words)
+# result = {word:len(word) for word in words}
+# print(result)
+
+# weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
+
+# weather_f ={day:(temp*9/5)+32 for (day,temp) in weather_c.items()} 
+
+# print(weather_f)
