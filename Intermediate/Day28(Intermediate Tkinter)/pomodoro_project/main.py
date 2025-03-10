e1599ad2343ec2b440f73fba073849d1b3402098
Barry Keith Salmon <salmon.barry1@gmail.com>
2025-03-10 15:28:27 +0000

    Completed countdown mechanism

--- Intermediate/Day28(Intermediate Tkinter)/pomodoro_project/main.py
+++ Intermediate/Day28(Intermediate Tkinter)/pomodoro_project/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -13,25 +14,38 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def start_timer():
+    count_down(1*60)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec == 0:
+        count_sec = "00"
+    elif count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text = f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count-1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file= "tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(row=2, column=2)
 
 timer_label = Label(text="Timer", font=(FONT_NAME, 36, "bold"), fg=GREEN, bg=YELLOW)
 timer_label.grid(row= 1, column= 2)
 
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-tomato_img = PhotoImage(file= "tomato.png")
-canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.grid(row=2, column=2)
-
 # start_button
-start_button = Button(text="Start", font=(FONT_NAME, 12, "bold"), )
+start_button = Button(text="Start", font=(FONT_NAME, 12, "bold"), command= start_timer)
 start_button.grid(row= 3, column= 1)
 
 # reset_button
@@ -41,8 +55,5 @@ reset_button.grid(row= 3, column= 3)
 # status_checks
 checkmark = Label(text="✓", font=(FONT_NAME, 24, "bold"), fg=GREEN, bg=YELLOW)
 checkmark.grid(row= 4, column= 2)
-
-
-
 
 window.mainloop()
