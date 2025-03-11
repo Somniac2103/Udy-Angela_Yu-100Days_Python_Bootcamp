dc4506d47256f8c8b20a44faa1a5620d6b8b5a1d
Barry Keith Salmon <salmon.barry1@gmail.com>
2025-03-11 16:16:34 +0000

    Corrected timer

--- Intermediate/Day28(Intermediate Tkinter)/pomodoro_project/main.py
+++ Intermediate/Day28(Intermediate Tkinter)/pomodoro_project/main.py
@@ -6,16 +6,26 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 2
+SHORT_BREAK_MIN = 1
+LONG_BREAK_MIN = 3
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 def start_timer():
-    count_down(1*60)
+    global reps
+    reps += 1
+
+    if reps % 8 == 0:
+        count_down(LONG_BREAK_MIN*5)
+    elif reps % 2 == 0:
+        count_down(SHORT_BREAK_MIN*5) 
+    else:
+        count_down(WORK_MIN*5) 
+          
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
@@ -29,8 +39,11 @@ def count_down(count):
     canvas.itemconfig(timer_text, text = f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count-1)
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
