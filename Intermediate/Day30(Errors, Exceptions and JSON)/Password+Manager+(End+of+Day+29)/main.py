fbf0c271b49f546096a61b16e996b800abf14458
Barry Keith Salmon <salmon.barry1@gmail.com>
2025-03-25 13:26:13 +0000

    Working with json files in python

--- Intermediate/Day30(Errors, Exceptions and JSON)/Password+Manager+(End+of+Day+29)/main.py
+++ Intermediate/Day30(Errors, Exceptions and JSON)/Password+Manager+(End+of+Day+29)/main.py
@@ -39,10 +39,17 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        with open("data.json", "r") as data_file:
-            # json.dump(new_data, data_file, indent=4)
-            data = json.load(data_file)
-            print(data)
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
