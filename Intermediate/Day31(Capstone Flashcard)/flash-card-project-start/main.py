968d6f6e3ae4120ff3ba8e8a6f3870c6d2e5714e
Barry Keith Salmon <salmon.barry1@gmail.com>
2025-03-28 12:54:50 +0000

    Completed gui layout

--- Intermediate/Day31(Capstone Flashcard)/flash-card-project-start/main.py
+++ Intermediate/Day31(Capstone Flashcard)/flash-card-project-start/main.py
@@ -13,8 +13,26 @@ window.config(padx=50, pady=50, bg= BACKGROUND_COLOR)
 
 canvas = Canvas(height=526, width=800, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_back_img = PhotoImage(file="./images/card_back.png")
-card_front_img = PhotoImage(file="./images/card_back.png")
+card_front_img = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
 canvas.grid(row=0, column=1, columnspan=2)
 
+language_text = canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
+
+word_text = canvas.create_text(400, 263, text="trouve", font=("Ariel", 60, "bold"))
+
+check_image = PhotoImage(file="./images/right.png")
+right_button = Button(image=check_image, highlightthickness=0, bd = 0)
+right_button.grid(row=2, column=1)
+
+cross_image = PhotoImage(file="./images/wrong.png")
+wrong_button = Button(image=cross_image, highlightthickness=0, bd = 0)
+wrong_button.grid(row=2, column=2)
+
+
+
+
+
 window.mainloop()
+
+
