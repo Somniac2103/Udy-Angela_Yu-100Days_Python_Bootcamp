e47bb877f012c12be1099d0b378340b66254f16a
Barry Keith Salmon <salmon.barry1@gmail.com>
2025-03-31 15:01:58 +0100

    Completed logic for flashcard

--- Intermediate/Day31(Capstone Flashcard)/flash-card-project-start/main.py
+++ Intermediate/Day31(Capstone Flashcard)/flash-card-project-start/main.py
@@ -1,9 +1,33 @@
 from tkinter import *
 from tkinter import messagebox
 from random import *
+import pandas
+import random
 import json
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+# ---------------------------- FUNCTIONS ------------------------------ # 
+error_message_dict = {}
+# load data
+def load_data():
+    try:
+        data = pandas.read_csv("./data/french_words.csv")
+        print(data)
+    except FileNotFoundError:
+       error_message_dict["datafile"] = "No Data file found or unable to load."
+       print(error_message_dict)
+    else:       
+        langauge_dict = {row.French: row.English for (index, row) in data.iterrows()}
+        print(langauge_dict)       
+        selected_french_word, selected_english_word = random.choice(list(langauge_dict.items()))
+        print(selected_french_word)
+        print(selected_english_word)
+
+
+
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -22,16 +46,12 @@ language_text = canvas.create_text(400, 150, text="French", font=("Ariel", 40, "
 word_text = canvas.create_text(400, 263, text="trouve", font=("Ariel", 60, "bold"))
 
 check_image = PhotoImage(file="./images/right.png")
-right_button = Button(image=check_image, highlightthickness=0, bd = 0)
+right_button = Button(image=check_image, highlightthickness=0, bd = 0, command=load_data)
 right_button.grid(row=2, column=1)
 
 cross_image = PhotoImage(file="./images/wrong.png")
-wrong_button = Button(image=cross_image, highlightthickness=0, bd = 0)
+wrong_button = Button(image=cross_image, highlightthickness=0, bd = 0, command=load_data)
 wrong_button.grid(row=2, column=2)
-
-
-
-
 
 window.mainloop()
 
