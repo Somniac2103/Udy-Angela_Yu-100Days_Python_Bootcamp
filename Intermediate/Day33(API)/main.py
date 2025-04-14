64ec87815c81b32c279622d36bbcac24b70ef477
Barry Keith Salmon <salmon.barry1@gmail.com>
2025-04-14 13:03:11 +0100

    Continue with request lib

--- Intermediate/Day33(API)/main.py
+++ Intermediate/Day33(API)/main.py
@@ -1,4 +1,29 @@
 import requests
+
+MY_LAT = 51.507351
+MY_LONG = -0.12778
  
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+
+# data = response.json()
+
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+
+# iss_position = (longitude, latitude)
+
+# print(iss_position)
+
+parameter = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameter)
+response.raise_for_status()
+data = response.json()
+sunrise =data["results"]["sunrise"]
+sunset =data["results"]["sunset"]
+print(sunrise)
+print(sunset)
