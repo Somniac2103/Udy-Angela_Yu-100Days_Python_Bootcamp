85edb21eb5e9dd14aeb13ac7e3ae5c1f61f8084c
Barry Keith Salmon <salmon.barry1@gmail.com>
2025-04-16 13:05:55 +0100

    completed day 33

--- Intermediate/Day33(API)/main.py
+++ Intermediate/Day33(API)/main.py
@@ -1,11 +1,17 @@
 import requests
 from datetime import datetime
+import smtplib
+
+#email credentials
+my_email = "barrysalmon36@gmail.com"
+app_password = "zxxb nxnh xddr kitr"
 
 # My Position
-MY_LAT = 51.507351
-MY_LONG = -0.127758
+MY_LAT = 50
+MY_LONG = -65
 
-print(f'{MY_LONG}, {MY_LAT}')
+print("My lats and longs")
+print(f'{MY_LAT}, {MY_LONG}')
 
 # ISS Position
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
@@ -15,9 +21,8 @@ data = response.json()
 iss_longitude = float(data["iss_position"]["longitude"])
 iss_latitude = float(data["iss_position"]["latitude"])
 
-iss_position = (iss_longitude, iss_latitude)
-
-print(iss_position)
+print("ISS position")
+print(f"{iss_latitude}, {iss_longitude}")
 
 # Night time at my position
 parameter = {
@@ -32,13 +37,13 @@ data = response.json()
 sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
 sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-print(sunrise)
-print(sunset)
+print(f'Local Sunrise = {sunrise}')
+print(f'Local Sunset = {sunset}')
 
 # My Current Time
 time_now = datetime.now().hour
 
-print(time_now)
+print(f'Current Local Time = {time_now}')
 
 # check if ISS and my position is within 5 degrees
 MY_LONG_upper_limit = MY_LONG + 5
@@ -47,10 +52,20 @@ MY_LAT_upper_limit = MY_LAT + 5
 MY_LAT_lower_limit = MY_LAT - 5
 
 if iss_latitude <= MY_LAT_upper_limit and  iss_latitude >= MY_LAT_lower_limit:
+    print("ISS within latitude tolerance")
     if iss_longitude <= MY_LONG_upper_limit and iss_longitude >= MY_LONG_lower_limit:
-
+        print("ISS within lonogtitude tolerance")
         # check if it is dark if within 5 degrees
-        if time_now <= sunrise or time_now <= sunset:
-            
+        if time_now < sunrise or time_now > sunset:
+            print("it is between sunset and sunrise at local position")
             # send email if wthin 5 degrees and dark
+            with smtplib.SMTP("smtp.gmail.com") as connection:
+                connection.starttls()
+                connection.login(user=my_email, password=app_password)
+                connection.sendmail(
+                    from_addr = my_email,
+                    to_addrs="salmon.barry1@gmail.com", 
+                    msg=f"Subject:ISS Alert\n\n The ISS is overhead and can be seen"
+                )
+            print("Email sent")
 
