e30f14e559aac2aebc3e877c2cb6c09ea92909c0
Barry Keith Salmon <salmon.barry1@gmail.com>
2025-05-12 13:05:39 +0100

    completed stock api commence with news api

--- Intermediate/Day36(Stock Trading Project)/main.py
+++ Intermediate/Day36(Stock Trading Project)/main.py
@@ -2,17 +2,17 @@ import os
 import requests
 from datetime import datetime, timedelta
 
-date = datetime.today().date()
+date = datetime.today().date() - timedelta(days=3)
 yesterday = date - timedelta(days=1)
 day_before_yesterday = yesterday - timedelta(days=1)
-# print(date)
-# print(yesterday)
-# print(day_before_yesterday)
 
+string_date = str(date)
+string_yesterday = str(yesterday)
+string_day_before_yesterday = str(day_before_yesterday)
 
 ALPHA_API_KEY = os.environ.get("ALPHA_API_KEY")
 STOCK_NAME = "TSLA"
-COMPANY_NAME = "Tesla Inc"
+
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 stock_params ={
@@ -22,33 +22,40 @@ stock_params ={
     "outputsize" : 'compact'
 }
 
-response = requests.get(STOCK_ENDPOINT, params=stock_params)
-data = response.json()
-# print(data['Time Series (Daily)'])
+stock_response = requests.get(STOCK_ENDPOINT, params=stock_params)
+stock_data = stock_response.json()
+
+price_change = float(stock_data['Time Series (Daily)'][string_date]['4. close']) - float(stock_data['Time Series (Daily)'][string_yesterday]['4. close'])
+print(price_change)
+price_change_percentage = (price_change / float(stock_data['Time Series (Daily)'][string_date]['4. close'])) *100
+print(price_change_percentage)
+
+if abs(price_change_percentage) > 5:
+    print("news")
+else:
+    print("nothing to report")
 
 
 
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+COMPANY_NAME = "Tesla Inc"
+NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
+
+news_params ={
+    "q" : {COMPANY_NAME},
+    "from" : {string_yesterday},
+    "sortedBy" : 'publishedAt',
+    "apiKey" : {NEWS_API_KEY}
+}
+
+news_response = requests.get("https://newsapi.org/v2/everything?q=tesla&from=2025-04-12&sortBy=publishedAt&apiKey=7e01424168404b859dd8d907a33e02ab")
 
 
+# news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+news_data = news_response.json()
 
+print(news_data)
 
-
-
-
-
-    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
-#TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
-
-#TODO 2. - Get the day before yesterday's closing stock price
-
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
-
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
 
     ## STEP 2: https://newsapi.org/ 
     # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
