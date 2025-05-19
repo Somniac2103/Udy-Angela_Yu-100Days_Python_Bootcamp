71584ba273fb652500887db75351103bfccbeb18
Barry Keith Salmon <salmon.barry1@gmail.com>
2025-05-19 13:55:59 +0100

    continue with pixela project

--- Intermediate/Day37(API Posts Requests & Headers)/main.py
+++ Intermediate/Day37(API Posts Requests & Headers)/main.py
@@ -8,6 +8,7 @@ PIXELA_WEBSITE = "https://pixe.la"
 PIXELA_USERNAME= os.getenv("PIXELA_USERNAME")
 PIXELA_API_TOKEN = os.getenv("PIXELA_API_TOKEN")
 
+# CREATE USER ---->
 PIXELA_USER_ENDPOINT = PIXELA_WEBSITE + "/v1/users"
 user_params = {
     "token" : PIXELA_API_TOKEN,
@@ -15,5 +16,36 @@ user_params = {
     "agreeTermsOfService"  : 'yes',
     "notMinor" : "yes"
 }
-
 # response = requests.post(url=PIXELA_USER_ENDPOINT, json=user_params)
+# print(response.text)
+
+
+# MAKE GRAPH ----->
+PIXELA_GRAPH_ENDPOINT = f'{PIXELA_WEBSITE}/v1/users/{PIXELA_USERNAME}/graphs'
+graph_params = {
+    "id" : "graph1",
+    "name" : "Cycling Graph",
+    "unit" : "Km",
+    "type" : "float",
+    "color" : "ajisai"
+}
+
+graph_headers = {
+    "X-USER-TOKEN" : PIXELA_API_TOKEN
+}
+# response = requests.post(url=PIXELA_GRAPH_ENDPOINT, json=graph_params, headers=graph_headers)
+# print(response.text)
+
+# RECORD DATA ----->
+PIXELA_GRAPH_NAME = "graph1"
+PIXELA_DATA_ENDPOINT = f'{PIXELA_WEBSITE}/v1/users/{PIXELA_USERNAME}/graphs/{PIXELA_GRAPH_NAME}'
+data_body = {
+    "date" : "20250519",
+    "quantity" : "9.0",
+}
+
+data_headers = {
+    "X-USER-TOKEN" : PIXELA_API_TOKEN
+}
+response = requests.post(url=PIXELA_DATA_ENDPOINT, headers=data_headers, data=data_body)
+print(response.text)
