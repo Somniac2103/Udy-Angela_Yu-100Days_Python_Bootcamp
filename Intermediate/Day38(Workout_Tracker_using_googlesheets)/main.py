a089fcf6983155d790e0fafb7e45560c6471a053
Barry Keith Salmon <salmon.barry1@gmail.com>
2025-05-22 17:23:30 +0100

    Prrogress in workout tracker

--- Intermediate/Day38(Workout_Tracker_using_googlesheets)/main.py
+++ Intermediate/Day38(Workout_Tracker_using_googlesheets)/main.py
@@ -27,3 +27,40 @@ nutritionix_headers = {
 response = requests.post(url=NUTRITIONIX_ENDPOINT,headers=nutritionix_headers, json=nutritionix_params)
 print(response.status_code)
 print(response.text)
+
+# SHEETY POST ----->
+SHEETY_API_WEBSITE = "https://api.sheety.co/"
+SHEETY_ACCOUNT = "64c5e842b391c3c5f830594bcee9e8fb"
+SHEETY_ACCOUNT_SHEET = "sheet1"
+
+SHEETY_ENDPOINT = F"{SHEETY_API_WEBSITE}{SHEETY_ACCOUNT}/workoutTracking/{SHEETY_ACCOUNT_SHEET}"
+
+nutritionix_params = {
+    "query"  : 'I ran 1 mile',
+    "weight_kg" : 90,
+    "height_cm" : 176,
+    "age" : 40
+}
+
+nutritionix_headers = {
+     'x-app-id' : NUTRITIONIX_API_APP_ID,
+     'x-app-key' : NUTRITIONIX_API_APP_KEYS
+}
+response = requests.post(url=SHEETY_ENDPOINT,headers=nutritionix_headers, json=nutritionix_params)
+print(response.status_code)
+print(response.text)
+
+  let body = {
+    sheet1: {
+      ...
+    }
+  }
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(body)
+  })
+  .then((response) => response.json())
+  .then(json => {
+    // Do something with object
+    console.log(json.sheet1);
+  });
