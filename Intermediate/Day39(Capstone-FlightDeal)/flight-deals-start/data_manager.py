16e28c6947a89a7f161d65615214da4349f9821c
Barry Keith Salmon <salmon.barry1@gmail.com>
2025-06-02 13:05:06 +0100

    Continue with capstone porject

--- Intermediate/Day39(Capstone-FlightDeal)/flight-deals-start/data_manager.py
+++ Intermediate/Day39(Capstone-FlightDeal)/flight-deals-start/data_manager.py
@@ -1,3 +1,27 @@
+import os
+import requests
+from dotenv import load_dotenv
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):   
+
+        load_dotenv()
+
+        self.SHEETY_API_URL = "https://api.sheety.co/"
+        self.SHEETY_ACCOUNT = os.getenv("SHEETY_ACCOUNT")
+        self.SHEETY_PROJECT = "flightDeals"
+        self.SHEETY_ACCOUNT_SHEET = "prices"
+
+        self.SHEETY_ENDPOINT = f"{self.SHEETY_API_URL}/{self.SHEETY_ACCOUNT}/{self.SHEETY_PROJECT}/{self.SHEETY_ACCOUNT_SHEET}"
+
+    def get_prices(self):
+        try:
+            response = requests.get(url=self.SHEETY_ENDPOINT)
+            print(response.status_code)
+            print(response.text)
+            data = response.json()
+            results = data.get("prices", [])
+            return (results)
+        except requests.RequestException as e:
+            print(f"[ERROR] Request Failed: {e}")
+            return []
--- Intermediate/Day39(Capstone-FlightDeal)/flight-deals-start/main.py
+++ Intermediate/Day39(Capstone-FlightDeal)/flight-deals-start/main.py
@@ -1,31 +1,19 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 import os
-import requests
 from dotenv import load_dotenv
+from data_manager import DataManager
+from flight_search import FlightSearch
 
 load_dotenv()
 
-SHEETY_API_URL = "https://api.sheety.co/"
-SHEETY_ACCOUNT = os.getenv("SHEETY_ACCOUNT")
-SHEETY_PROJECT = "flightDeals"
-SHEETY_ACCOUNT_SHEET = "prices"
+dm = DataManager()
+fs = FlightSearch()
 
-SHEETY_ENDPOINT = f"{SHEETY_API_URL}/{SHEETY_ACCOUNT}/{SHEETY_PROJECT}/{SHEETY_ACCOUNT_SHEET}"
+prices = dm.get_prices()
 
+for item in prices:
+    if item["iataCode"] is None:
+        item["iataCode"] = fs.get_code(item["city"])
 
-# sheety_params = {
-#     "query"  : exercise_input,
-#     "weight_kg" : 90,
-#     "height_cm" : 176,
-#     "age" : 40
-# }
-
-# sheety_headers = {
-#      'x-app-id' : NUTRITIONIX_API_APP_ID,
-#      'x-app-key' : NUTRITIONIX_API_APP_KEYS
-# }
-response = requests.get(url=SHEETY_ENDPOINT)
-print(response.status_code)
-print(response.text)
 
 
